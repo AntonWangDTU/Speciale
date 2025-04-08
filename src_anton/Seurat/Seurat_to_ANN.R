db16798620fa55bf9160b79003c17ed6e04505d8
aws <s183220@dtu.dk>
2025-04-08 16:57:10 +0200

    new

--- src_anton/Seurat/Seurat_to_ANN.R
+++ src_anton/Seurat/Seurat_to_ANN.R
@@ -1,21 +1,38 @@
-# Define the grid for x and y values
-x <- seq(-3, 3, length.out = 30)
-y <- seq(-3, 3, length.out = 30)
 
-# Create a meshgrid (grid of all combinations of x and y)
-grid <- expand.grid(x = x, y = y)
+library(Seurat)
+library(patchwork)
+library(ggplot2)
+library(tidyverse)
 
-# Define a bivariate normal distribution (independent variables)
-z <- dnorm(grid$x) * dnorm(grid$y)  # Normal distribution on both axes
 
-# Reshape z into a matrix suitable for the 'persp' function
-z_matrix <- matrix(z, nrow = length(x), ncol = length(y))
 
-# Create a 3D plot using persp
-persp(x, y, z_matrix, 
-      main = "3D Normal Distribution", 
-      xlab = "X", 
-      ylab = "Y", 
-      zlab = "Density", 
-      col = "lightblue", 
-      theta = 30, phi = 30)
+# Get system info to determine data path
+sys_info <- Sys.info()
+
+if (grepl("Linux", sys_info["sysname"])) {
+  data_dir <- '../../../data/'
+} else if (sys_info["sysname"] == "Windows") {
+  data_dir <- 'C:/Users/hostp/Desktop/data/'
+}
+
+# Path to saved Seurat object
+seurat_path <- file.path(data_dir, "cao_seurat.rds")
+
+# Check if the object already exists
+message("Loading existing Seurat object...")
+cao <- readRDS(seurat_path)
+
+
+
+
+glimpse(test)
+
+
+
+
+
+
+
+
+
+
